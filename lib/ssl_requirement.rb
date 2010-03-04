7fd8fbbbf986410297e6d66843d00e2a9c19a94f
Doug Hudson <github@fishcrime.org>
2010-03-04 10:47:25 -0500

    Supports :all for ssl_allowed and ssl_required

--- lib/ssl_requirement.rb
+++ lib/ssl_requirement.rb
@@ -38,11 +38,13 @@ module SslRequirement
   protected
     # Returns true if the current action is supposed to run as SSL
     def ssl_required?
-      (self.class.read_inheritable_attribute(:ssl_required_actions) || []).include?(action_name.to_sym)
+#      (self.class.read_inheritable_attribute(:ssl_required_actions) || []).include?(action_name.to_sym)
+      (self.class.read_inheritable_attribute(:ssl_required_actions) || []).any? { |a| a == :all || a == action_name.to_sym }
     end
     
     def ssl_allowed?
-      (self.class.read_inheritable_attribute(:ssl_allowed_actions) || []).include?(action_name.to_sym)
+#      (self.class.read_inheritable_attribute(:ssl_allowed_actions) || []).include?(action_name.to_sym)
+      (self.class.read_inheritable_attribute(:ssl_allowed_actions) || []).any? { |a| a == :all || a == action_name.to_sym }
     end
 
   private
